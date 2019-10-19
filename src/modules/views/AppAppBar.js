b602f1d8178bf4eb9455e2b804bfc2fd41bb093f
Ehtesham SIddiqui <esiddiqui0530@gmail.com>
2019-10-19 17:52:19 +0530

    Demo how to use react router

--- src/modules/views/AppAppBar.js
+++ src/modules/views/AppAppBar.js
@@ -1,38 +1,39 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 // import clsx from 'clsx';
-import { withStyles } from '@material-ui/core/styles';
-import Link from '@material-ui/core/Link';
-import AppBar from '../components/AppBar';
-import Toolbar, { styles as toolbarStyles } from '../components/Toolbar';
+import { withStyles } from "@material-ui/core/styles";
+import Link from "@material-ui/core/Link";
+import AppBar from "../components/AppBar";
+import Toolbar, { styles as toolbarStyles } from "../components/Toolbar";
+import { Link as PageLink } from "react-router-dom";
 
 const styles = theme => ({
   title: {
-    fontSize: 24,
+    fontSize: 24
   },
   placeholder: toolbarStyles(theme).root,
   toolbar: {
-    justifyContent: 'space-between',
+    justifyContent: "space-between"
   },
   left: {
-    flex: 1,
+    flex: 1
   },
   leftLinkActive: {
-    color: theme.palette.common.white,
+    color: theme.palette.common.white
   },
   right: {
     flex: 1,
-    display: 'flex',
-    justifyContent: 'flex-end',
+    display: "flex",
+    justifyContent: "flex-end"
   },
   rightLink: {
     fontSize: 16,
     color: theme.palette.common.white,
-    marginLeft: theme.spacing(3),
+    marginLeft: theme.spacing(3)
   },
   linkSecondary: {
-    color: theme.palette.secondary.main,
-  },
+    color: theme.palette.secondary.main
+  }
 });
 
 function AppAppBar(props) {
@@ -50,7 +51,7 @@ function AppAppBar(props) {
             className={classes.title}
             //href="/premium-themes/onepirate/"
           >
-            {'Central Sweets'}
+            {"Central Sweets"}
           </Link>
           {/* <div className={classes.right}>
             <Link
@@ -73,13 +74,14 @@ function AppAppBar(props) {
           </div> */}
         </Toolbar>
       </AppBar>
+      <PageLink to="/contact">Contact</PageLink>
       <div className={classes.placeholder} />
     </div>
   );
 }
 
 AppAppBar.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(AppAppBar);
